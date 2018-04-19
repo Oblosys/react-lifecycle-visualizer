625c995da35489c28fa0d897be94fd296b83673c
Oblosys <martijn@oblomov.com>
2018-04-19 12:53:43 +0200

    Disable redux-devtools binding for react-lifecycle store
    Otherwise it overrides the binding in application code. There's probably a way to specify a different store key for the lib store, but it doesn't seem worth the effort at the moment.

--- src/VisualizerProvider.jsx
+++ src/VisualizerProvider.jsx
@@ -1,19 +1,17 @@
 import * as React from 'react';
 import { createProvider } from 'react-redux';
-import { applyMiddleware, compose, createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 
 import * as constants from './constants';
 import * as actionCreators from './actionCreators';
 import { reducer } from './reducer';
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const Provider = createProvider(constants.reduxStoreKey);
 
 const store = createStore(
   reducer,
-  composeEnhancer(applyMiddleware(thunk))
+  applyMiddleware(thunk)
 );
 
 export const clearLog = () => store.dispatch(actionCreators.clearLog());
