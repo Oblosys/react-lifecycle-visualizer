90cb2bfd98f19142a06c57b288ad018fcb589d2a
Oblosys <martijn@oblomov.com>
2018-04-21 19:28:43 +0200

    Move example to examples/parent-child-demo

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const devServerPort = 8001;
 
 module.exports = {
-  entry: path.join(__dirname, 'examples/src/index.js'),
+  entry: path.join(__dirname, 'examples/parent-child-demo/src/index.js'),
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'client-bundle.js',
